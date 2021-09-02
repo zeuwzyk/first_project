b7c4a8c4e0c5fcd10f118396b412ed1d3338b2f3
Saniok <mamygas@yahoo.com>
2021-09-02 12:42:25 +0300

    c# project

--- example1/Program.cs
+++ example1/Program.cs
@@ -6,24 +6,38 @@ namespace example1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter number of rows matrix: ");
-            int numRows = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter number of columns matrix: ");
-            int numColumns = Convert.ToInt32(Console.ReadLine());
+            int numRows = 0;
+            int numColumns = 0;
+
+            Console.Write("Enter integer number of rows matrix: ");
+            numRows = verification(numRows);
+
+            Console.Write("Enter integer number of columns matrix: ");
+            numColumns = verification(numColumns);
 
             int[,] matrix = new int[numRows, numColumns];
 
-            Console.Write("\nMatrix:\n"); 
+            Console.Write("\nMatrix:\n");
             Random rand = new Random();
-            for (int i=0; i < numRows; i++)
+            for (int i = 0; i < numRows; i++)
             {
-                for (int j=0; j<numColumns; j++) 
-                { 
-                    matrix[i, j] = rand.Next(10,99);
+                for (int j = 0; j < numColumns; j++)
+                {
+                    matrix[i, j] = rand.Next(10, 99);
                     Console.Write($"{matrix[i, j]} ");
                 }
                 Console.Write("\n");
-            }            
+            }
+        }
+        static int verification(int value)
+        {
+            while (!int.TryParse(Console.ReadLine(), out value) && value < 2147483647
+                && value > -2147483648)
+            {
+                Console.WriteLine("You entered inavlid number or not number");
+                Console.Write("\nEnter number of rows matrix: ");
+            }
+            return value;
         }
     }
 }
