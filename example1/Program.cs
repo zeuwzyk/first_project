4e38cfa742464c4459c49a5986d00fa07e64bcb0
Saniok <mamygas@yahoo.com>
2021-09-02 10:58:38 +0300

    the first project

---
+++ example1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace example1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number of rows matrix: ");
+            int numRows = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number of columns matrix: ");
+            int numColumns = Convert.ToInt32(Console.ReadLine());
+
+            int[,] matrix = new int[numRows, numColumns];
+
+            Console.Write("\nMatrix:\n"); 
+            Random rand = new Random();
+            for (int i=0; i < numRows; i++)
+            {
+                for (int j=0; j<numColumns; j++) 
+                { 
+                    matrix[i, j] = rand.Next(10,99);
+                    Console.Write($"{matrix[i, j]} ");
+                }
+                Console.Write("\n");
+            }            
+        }
+    }
+}
