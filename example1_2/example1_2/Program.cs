f715ff9e09aa45dead61e9f62ba69452cb087481
zeuwzyk <72517469+zeuwzyk@users.noreply.github.com>
2021-09-08 14:35:48 +0300

    08/09

---
+++ example1_2/example1_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace example1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter information: ");
+            string information = Console.ReadLine();
+            string[] words = information.Split(' ');
+            //если размер вордс больше 3х работаем еще раз
+            int numRows, numColumns;
+
+            for (int i=0; i < words.Length; i++)//цикл не нужен работаем до 3х значений
+            {
+                if (int.TryParse(words[i], out numRows))
+                    Console.WriteLine("int ", numRows);
+            }
+
+        }                
+    }
+}
